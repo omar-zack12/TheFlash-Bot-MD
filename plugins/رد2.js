5aa7d0712faf8f934e8712d8ea2f2c8dba74e349
Araab Zack <162893646+araab-zack@users.noreply.github.com>
2024-09-25 03:42:32 +0300

    Update رد2.js

--- plugins/رد2.js
+++ plugins/رد2.js
@@ -2,8 +2,8 @@ let handler = async (m, { conn }) => {
     let user = global.db.data.users[m.sender];
     let name = conn.getName(m.sender);
     let taguser = '@' + m.sender.split("@s.whatsapp.net")[0];
-    let message = `*مــنــور يـا قــلــب تــوغــا لــي الـاوامــر اكــتــب (.اوامر) 🌸*`;
-await conn.sendMessage(m.chat, { react: { text: '🌸', key: m.key } })
+    let message = `*مــنــور يـا قــلــب فــلاش لــي الـاوامــر اكــتــب (.اوامر) ⚡*`;
+await conn.sendMessage(m.chat, { react: { text: '⚡', key: m.key } })
     conn.sendFile(m.chat, 'https://envs.sh/wBC.jpg', 'image.jpg', message, m);
 };
 

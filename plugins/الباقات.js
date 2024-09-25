13223c11ff8b2a1a9e4969268d6c1695c8eb2fb6
Araab Zack <162893646+araab-zack@users.noreply.github.com>
2024-09-25 05:45:38 +0300

    Update الباقات.js

--- plugins/الباقات.js
+++ plugins/الباقات.js
@@ -25,7 +25,7 @@ const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, is
     let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
     let taguser = '@' + m.sender.split("@s.whatsapp.net")[0];
   await conn.sendMessage(m.chat, { react: { text: '🚀', key: m.key } })
-  const harley = 'https://i.imgur.com/Y0zUIJT.jpeg'
+  const harley = 'https://envs.sh/06H.jpg'
   const mentionId = m.key.participant || m.key.remoteJid;
  
 conn.relayMessage(m.chat, { viewOnceMessage: { message: { interactiveMessage: { header: { title: `harley`}, body: { text: `

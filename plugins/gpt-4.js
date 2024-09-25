ebb1400566dd50fae90ea897cfd923fcba780441
Araab Zack <162893646+araab-zack@users.noreply.github.com>
2024-09-25 22:59:01 +0300

    Update gpt-4.js

--- plugins/gpt-4.js
+++ plugins/gpt-4.js
@@ -19,7 +19,7 @@ let handler = async (m, { conn, text, args, usedPrefix, command }) => {
       m.chat, 
       replyText, 
       author, 
-      'https://envs.sh/wHc.jpg', 
+      'https://envs.sh/06H.jpg', 
       [['الــمــطـور', `.المطور`]], 
       null, 
       [['قــنـاة الــبــوت', `https://whatsapp.com/channel/0029ValRCUIFi8xmK9xBOr0I`]], 

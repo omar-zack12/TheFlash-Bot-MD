482067ba4a922bf726c6daa5e53d2c30b7dc2c72
Araab Zack <162893646+araab-zack@users.noreply.github.com>
2024-09-25 04:05:56 +0300

    Update رد2.js

--- plugins/رد2.js
+++ plugins/رد2.js
@@ -2,13 +2,9 @@ let handler = async (m, { conn }) => {
     let user = global.db.data.users[m.sender];
     let name = conn.getName(m.sender);
     let taguser = '@' + m.sender.split("@s.whatsapp.net")[0];
-    
-    let message = 'مــنــور يـا قــلــب فــلاش لــي الـاوامــر اكــتــب (.اوامر) ⚡️';
-    await conn.sendMessage(m.chat, { react: { text: '⚡️', key: m.key } });
-    await conn.sendFile(m.chat, 'https://envs.sh/wHc.jpg', 'image.jpg', message, m);
-
-    let audioPath = './path/audio/audio.mp3';
-    await conn.sendMessage(m.chat, { audio: { url: audioPath }, mimetype: 'audio/mp4', ptt: true });
+    let message = '*مــنــور يـا قــلــب فــلاش لــي الـاوامــر اكــتــب (.اوامر) ⚡️*';
+await conn.sendMessage(m.chat, { react: { text: '⚡️', key: m.key } })
+    conn.sendFile(m.chat, 'https://envs.sh/wHc.jpg', 'image.jpg', message, m);
 };
 
 handler.customPrefix = /^بوت$/i;

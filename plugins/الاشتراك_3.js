cbaa00ba8c7091786940e1e438fa3dbb01584bb0
Araab Zack <162893646+araab-zack@users.noreply.github.com>
2024-09-25 18:24:02 +0300

    Update الاشتراك_3.js

--- plugins/الاشتراك_3.js
+++ plugins/الاشتراك_3.js
@@ -4,7 +4,7 @@ const { prepareWAMessageMedia } = pkg;
 const handler = async (m, { conn }) => {
     await conn.sendMessage(m.chat, { react: { text: '🚀', key: m.key } });
 
-    const harley = 'https://i.imgur.com/jvFFC2k.jpeg';
+    const harley = 'https://envs.sh/06H.jpg';
 
     let messageContent = {
         viewOnceMessage: {
